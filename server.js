82450d7803d000e49607751e14f470bf8966b4b5
albi1V <albinvarghese2025@mca.ajce.in>
2024-11-10 13:39:46 +0530

    updation 1

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const path = require('path');
 require('dotenv').config(); 
 
 connectDB();
-app.use(cors({
-  origin: '*', // Adjust to specific Netlify URL if preferred
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
